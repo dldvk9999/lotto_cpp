878120abb134e94fe00b0a91eabfa5d9dc9a595a
jonggeun <dldvk9999@naver.com>
2022-02-04 16:03:41 +0900

    init setting

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+#include <random>
+#include <vector>
 using namespace std;
 
 int main(void) {
-	cout << "Hello" << endl;
+	// Random seed generator
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> dis(1, 45);
+
+	vector<int> num(5);
+
+	for (int index = 0; index < 5; index++) {
+		for (int i = 0; i < 5; i++) {
+			int tmp = dis(gen);
+			if (find(num.begin(), num.end(), tmp) != num.end()) i--;
+			else num[i] = tmp;
+		}
+		
+		sort(num.begin(), num.end());
+
+		for (int i = 0; i < 5; i++)
+			cout << num[i] << "\t";
+		cout << endl;
+	}
 	return 0;
 }
